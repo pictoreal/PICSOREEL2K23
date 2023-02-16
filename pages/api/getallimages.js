9ff21685f52c6d6312d38af7d90fff87a58289c1
Ashutosh Shaha <61512480+ashutosh264@users.noreply.github.com>
2023-02-17 04:19:39 +0530

    fix: admin removed from userlist & uploadimage removed

--- pages/api/getallimages.js
+++ pages/api/getallimages.js
@@ -1,9 +1,9 @@
 import clientPromise from "../../lib/mongodb";
 
 export default async function handler(req, res) {
-    const client = await clientPromise;
-    const db = client.db("Voting");
-    const name = req.query
-    const allPosts = await db.collection("Images").find({}).toArray();
-    res.status(200).json(allPosts);
+  const client = await clientPromise;
+  const db = client.db("Voting");
+  const name = req.query;
+  const allPosts = await db.collection("Images").find({}).toArray();
+  res.status(200).json(allPosts);
 }
