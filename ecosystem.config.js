3b8fd8cbeb21d231d5b74c123dec6525fead9911
Ayush-Gala <ayushgala2@gmail.com>
2023-02-16 13:15:01 +0530

    Final 3.0

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -12,7 +12,7 @@ module.exports = {
       repo : 'git@github.com:pictoreal/PICSOREEL2K23.git',
       path : '/home/ubuntu/PICSOREEL2K23',
       'pre-deploy-local': '',
-      'post-deploy' : 'source ~/.nvm/nvm.sh && npm install --legacy-peer -deps && npm run build && pm2 reload ecosystem.config.js --env production',
+      'post-deploy' : 'source ~/.nvm/nvm.sh && npm install --force && npm run build && pm2 reload ecosystem.config.js --env production',
       'pre-setup': '',
       'ssh_options': 'ForwardAgent=yes'
     }
