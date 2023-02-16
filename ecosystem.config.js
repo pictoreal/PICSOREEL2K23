4937155f5b5399cb4df598734b79e132c5c379fc
Ayush-Gala <ayushgala2@gmail.com>
2023-02-16 12:07:06 +0530

    ecosytem add

---
+++ ecosystem.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  apps : [{
+    script: 'npm start'
+  }],
+
+  deploy : {
+    production : {
+      key: 'key.pem',
+      user : 'ubuntu',
+      host : '18.235.233.206',
+      ref  : 'origin/main',
+      repo : 'git@github.com:pictoreal/PICSOREEL2K23.git',
+      path : '/home/ubuntu/PICSOREEL2K23',
+      'pre-deploy-local': '',
+      'post-deploy' : 'souce ~/.nvm/nvm.sh && npm install --force && npm run build && pm2 reload ecosystem.config.js --env production',
+      'pre-setup': '',
+      'ssh_options': 'ForwardAgent=yes'
+    }
+  }
+};
